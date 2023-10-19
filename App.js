47586f15b8a1f75390dae76489e575f63f525577
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-10-19 08:14:39 +0530

    Intro to Middlewares

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 //const http= require('http');
 const express=require('express');
+const bodyParser=require('body-parser')
 const app=express()
 //console.log(routes.sometext)
-app.use((req,res,next)=>{
-    console.log('Inn the middleware!')
-    next()
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Add Product"><br><input type="number" name="size" placeholder="Add Size"></input><br><button type="submit">Submit</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log('In another middleware!')
+app.post('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     res.send("<h1>Hello!!</h1>");
-    //res.send("{ key1: value }")
 })
 // const server=http.createServer(app)
 // server.listen(3000)
